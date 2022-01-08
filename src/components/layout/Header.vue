b74dfac89b43dcb228ce28e2d1f5bbbdf4426038
Angel <gutierrezramosjoseangel@gmail.com>
2022-01-07 19:13:29 -0600

    Added - Icon and search input element

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,11 +1,16 @@
 <template>
     <div id="header" class="header">
-        header
+        <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
+        <SearchInput />
     </div>
 </template>
 
 <script>
+import SearchInput from '../SearchInput.vue';
 export default {
-    
+    name: 'Header',
+    components: {
+        SearchInput
+    }
 }
 </script> 
