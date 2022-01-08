09755e785d34afacec9597363ccae9beeef304fa
Angel <gutierrezramosjoseangel@gmail.com>
2022-01-08 01:14:02 -0600

    Added - Pokemon List, grid system, pokemon card,

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,11 +1,32 @@
 <template>
-    <div id="pokemon-card">
-        <h1>Pokemon Card</h1>
+    <div id="pokemon-card" class="pokemon-card" v-if="pokemonInfo !== null">
+        <h5 class="pokemon-card__name">{{pokemon.name}}</h5>
+        <img class="pokemon-card__img" :src=pokemonInfo.sprites.front_default :alt="pokemon.name">
+        <p>{{pokemonInfo.stats[1].stat.name}} - {{pokemonInfo.stats[1].base_stat}}</p>
+        <p class="pokemon-card__types" v-for="(types, index) in pokemonInfo.types" v-bind:key="index">
+            {{types.type.name}}
+        </p>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    
+    name: 'PokemonCard',
+    props: {
+        pokemon: Object
+    },
+    data(){
+        return {
+            pokemonInfo: null
+        }
+    },
+    mounted() {
+        axios.get(this.pokemon.url).then(response => {
+            this.pokemonInfo = response.data;
+            console.log(this.pokemonInfo);
+        })
+    }
 }
 </script>
--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,11 +1,32 @@
 <template>
-    <div id="pokemon-list">
-        <h1>Pokemon List</h1>
+    <div id="pokemon-list" class="pokemon-list">
+        <h1 class="u-text-center">Pokemon List</h1>
+        <div class="row">
+            <div class="col-1-of-4" v-for="(pokemon, index) in data" v-bind:key="index">
+                <PokemonCard :pokemon="pokemon" :index="index" /> 
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import PokemonCard from '../components/PokemonCard.vue'
+
 export default {
-    
+    name: 'PokemonList',
+    components: {
+        PokemonCard
+    },
+    data() {
+        return {
+            data: []
+        }
+    },
+    mounted() {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=20').then(response => {
+            this.data = response.data.results;
+        })
+    }
 }
 </script>
