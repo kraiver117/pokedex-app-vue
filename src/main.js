fb7419b3d21a2e4c5902af5e8ed5edcf775997de
Angel <gutierrezramosjoseangel@gmail.com>
2022-01-09 00:15:03 -0600

    Added - Pokemon Details View and new font family

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import ColorThief from 'colorthief'
 
-Vue.use(axios)
+library.add(faArrowLeft)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(axios, ColorThief)
 Vue.config.productionTip = false
 
 new Vue({
